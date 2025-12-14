196b4f9c1616d8de22578d06d17978960a14bc81
HonzaTuron <jan.turon@apify.com>
2025-12-14 13:20:41 +0100

    feat: Add converter deps & config

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'indent': ['error', 4],
+      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
+    },
   },
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { promises as fs } from 'fs'
+import getRates from './src/queries/getRates'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      {
+        name: 'get-rates',
+        async buildStart() {
+          const rates = await getRates()
+          await fs.mkdir('src/rates', { recursive: true })
+          await fs.writeFile('src/rates/rates.json', JSON.stringify(rates, null, 2))
+        }
+      }
+  ],
 })
