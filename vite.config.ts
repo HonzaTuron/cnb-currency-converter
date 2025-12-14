f28e2359e43fde0435fe9c5ae02d9f5b2dc9959a
HonzaTuron <jan.turon@apify.com>
2025-12-14 13:21:10 +0100

    fix: Eslint

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import getRates from './src/queries/getRates'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      {
-        name: 'get-rates',
-        async buildStart() {
-          const rates = await getRates()
-          await fs.mkdir('src/rates', { recursive: true })
-          await fs.writeFile('src/rates/rates.json', JSON.stringify(rates, null, 2))
+    plugins: [
+        react(),
+        {
+            name: 'get-rates',
+            async buildStart() {
+                const rates = await getRates()
+                await fs.mkdir('src/rates', { recursive: true })
+                await fs.writeFile('src/rates/rates.json', JSON.stringify(rates, null, 2))
+            }
         }
-      }
-  ],
+    ],
 })
